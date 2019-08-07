65dc8738bc3262afec1e5512572d69f35e6c402d
Jeremy Tammik <jta@Jeremys-MacBook-Pro.local>
2019-08-07 20:49:40 +0200

    started working on EdgeLoopRetriever.GetLoops

--- ElementOutline/EdgeLoopRetriever.cs
+++ ElementOutline/EdgeLoopRetriever.cs
@@ -21,10 +21,34 @@ namespace ElementOutline
     Dictionary<int, JtLoops> _loops
       = new Dictionary<int, JtLoops>();
 
+    /// <summary>
+    /// Return loops for outer 2D outline 
+    /// of given element.
+    /// - Retrieve geometry curves from edges 
+    /// - Convert to linear segments and 2D integer coordinates
+    /// - Convert to non-intersecting line segments
+    /// - Start from left-hand bottom point
+    /// - Go down, then right
+    /// - Keep following right-mostconection until closed loop is found
+    /// </summary>
+    JtLoops GetLoops( Element e )
+    {
+      JtLoops loops = null;
+      return loops;
+    }
+
     public EdgeLoopRetriever( 
       Document doc,
       ICollection<ElementId> ids )
     {
+      foreach( ElementId id in ids )
+      {
+        Element e = doc.GetElement( id );
+
+        JtLoops loops = GetLoops( e );
+
+        _loops.Add( id.IntegerValue, loops );
+      }
     }
 
     public Dictionary<int, JtLoops> Loops
