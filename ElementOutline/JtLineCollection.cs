fb82c522daf7f062c87d88c4370751cb570d71a9
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-08-14 22:26:27 +0200

    started on JtLineCollection

---
+++ ElementOutline/JtLineCollection.cs
@@ -0,0 +1,17 @@
+﻿#region Namespaces
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+#endregion
+
+namespace ElementOutline
+{
+  /// <summary>
+  /// Store line segment data twice over, with both 
+  /// endpoints entered as keys, pointing to a list 
+  /// of all the corresponding other endpoints
+  /// </summary>
+  class JtLineCollection : Dictionary<Point2dInt, List<Point2dInt>>
+  {
+  }
+}
